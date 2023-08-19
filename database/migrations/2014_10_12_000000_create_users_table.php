2419624318850132cb42b6f878dc0f0efd5f9a79
Ashraf <shahadatashraf10@gmail.com>
2023-08-19 12:35:35 +0600

    added new column to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Services\GlobalStatus;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -12,13 +13,26 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id')->primary();
+            $table->string('name', 30);
+            $table->string('first_name', 100)->nullable();
+            $table->string('last_name', 100)->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('mobile', 20)->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('image', 50)->nullable();
+            $table->date('birth_date')->nullable();
+            $table->string('city', 255)->nullable();
+            $table->string('zip_code', 20)->nullable();
+            $table->string('address', 255)->nullable();
+            $table->string(
+                'record_status',
+                20
+            )->default(GlobalStatus::getRecordStatus('Active'));
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 

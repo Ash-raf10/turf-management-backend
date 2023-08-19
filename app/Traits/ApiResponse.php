d9d8b16100dba519e74b8031ea4cbba51f1af70d
Ashraf <shahadatashraf10@gmail.com>
2023-08-19 12:36:25 +0600

    changed data type of api response

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -8,7 +8,7 @@ trait ApiResponse
      * SendResponse send json response
      *
      * @param bool        $status     true/false
-     * @param array|string $data     data or result
+     * @param mixed $data     data or result
      * @param array|string $message    message to show to the user
      * @param int         $statusCode http status codes
      * @param int         $internalCode for our use
@@ -16,7 +16,7 @@ trait ApiResponse
      */
     protected function sendResponse(
         bool $status = true,
-        array|string $data = "",
+        mixed $data = "",
         array|string $message = "",
         int $statusCode = 200,
         $internalCode = 0000

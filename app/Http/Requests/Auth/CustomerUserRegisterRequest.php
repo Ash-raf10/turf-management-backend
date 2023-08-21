1b21ae2ccf6be292ca09fe04048de5fa3a58dacc
Ashraf <shahadatashraf10@gmail.com>
2023-08-22 02:05:03 +0600

    prepend +880 before mobile number

--- app/Http/Requests/Auth/CustomerUserRegisterRequest.php
+++ app/Http/Requests/Auth/CustomerUserRegisterRequest.php
@@ -16,6 +16,23 @@ class CustomerUserRegisterRequest extends FormRequest
     }
 
     /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        // if mobile field is present then check if it already contains +880
+        if ($this->mobile && strpos($this->mobile, "+880") !== 0) {
+            // if not then add +880
+            $phone = preg_replace('/^(0|88)/', '+880', $this->mobile, 1);
+
+
+            $this->merge([
+                'mobile' => $phone,
+            ]);
+        }
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
@@ -32,7 +49,10 @@ class CustomerUserRegisterRequest extends FormRequest
                     ->numbers()
                     ->symbols()
             ],
-            'mobile' => ['required', 'string', 'regex:/^(((\+|00)?880)|0)(\d){10}$/', 'max:20', 'min:11'],
+            'mobile' => [
+                'required', 'string', 'regex:/^(((\+|00)?880)|0)(\d){10}$/',
+                'max:20', 'min:11', 'unique:users,mobile,except,id'  //  mobile number should be unique
+            ],
             'city' => 'required|string|max:255'
         ];
     }

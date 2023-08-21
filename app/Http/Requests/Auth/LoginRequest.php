4de38f154c7f72347bcca38ad578626c214481e5
Ashraf <shahadatashraf10@gmail.com>
2023-08-22 02:05:53 +0600

    added login process with valid mobile

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -16,6 +16,28 @@ class LoginRequest extends FormRequest
     }
 
     /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        // check if the field contains email or mobile and set as mobile or email
+        $identifierType = filter_var($this->email_phone, FILTER_VALIDATE_EMAIL) ? 'email' : 'mobile';
+
+        $this->merge([
+            'identifier_type' => $identifierType,
+        ]);
+
+        // if field is mobile then add +880
+        if ($identifierType === 'mobile') {
+            $phone = preg_replace('/^(0|88)/', '+880', $this->email_phone, 1);
+
+            $this->merge([
+                'email_phone' => $phone,
+            ]);
+        }
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
@@ -23,14 +45,26 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email|max:255',
+            'email_phone' => [
+                'required',
+                'string',
+                function ($attribute, $value, $fail) {
+                    if (
+                        !filter_var($value, FILTER_VALIDATE_EMAIL)
+                        && !preg_match('/^(((\+|00)?880)|0)(\d){10}$/', $value)
+                    ) {
+                        $fail('This must be a valid email or phone number.');
+                    }
+                },
+            ],
             'password' => [
                 'required', Password::min(8)
                     ->letters()
                     ->mixedCase()
                     ->numbers()
                     ->symbols()
-            ]
+            ],
+            'identifier_type' => 'required'
         ];
     }
 }

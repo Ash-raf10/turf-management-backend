975630c9e9ca79bddb2a838831a9bb7707e43096
Ashraf <shahadatashraf10@gmail.com>
2023-08-25 00:09:15 +0600

    added otp table and model

---
+++ app/Models/Otp.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Otp extends Model
+{
+    use HasFactory;
+    use HasUuids;
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = [
+        'uuid'
+    ];
+}

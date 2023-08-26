f299b3497cb880d7827667b3b21a81341fe58cbf
Ashraf <shahadatashraf10@gmail.com>
2023-08-26 19:52:04 +0600

    added msg in internal response

--- app/Traits/InternalResponse.php
+++ app/Traits/InternalResponse.php
@@ -7,11 +7,13 @@ class InternalResponseObject
 {
     public bool $success;
     public mixed $data;
+    public string $msg;
 
-    public function __construct(bool $success, mixed $data)
+    public function __construct(bool $success, mixed $data, string $msg)
     {
         $this->success = $success;
         $this->data = $data;
+        $this->msg = $msg;
     }
 }
 
@@ -22,15 +24,18 @@ trait InternalResponse
      *
      * @param bool        $status     true/false
      * @param mixed $data     data or result
+     * @param string $msg  message
      * @return InternalResponseObject An Object containing result.
      *               - 'success'   : boool
      *               - 'data' : mixed  can be array|string|object
+     *               - 'message' : a meaningfull message if any
      */
     protected function response(
         bool $status = true,
         mixed $data = "",
+        string $msg = ""
     ): InternalResponseObject {
 
-        return new InternalResponseObject($status, $data);
+        return new InternalResponseObject($status, $data, $msg);
     }
 }

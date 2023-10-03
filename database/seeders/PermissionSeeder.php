c712bf3d0e57df49ff30708d52fd71c45f3f1108
Ashraf <shahadatashraf10@gmail.com>
2023-10-04 00:00:01 +0600

    added permission seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use Illuminate\Database\Seeder;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $ops = ['create', 'read', 'update', 'delete'];
+        $module = [
+            'user' => [
+                ...$ops,
+                'user'
+            ],
+        ];
+
+
+        foreach ($module as $mod => $permissions) {
+            foreach ($permissions as $permission) {
+                if (in_array($permission, $ops)) {
+                    $permission = $permission . '-' . $mod;
+                }
+                Permission::firstOrCreate([
+                    'name' => $permission,
+                    'module' => $mod,
+                    'guard_name' => "api",
+                ]);
+            }
+        }
+    }
+}
